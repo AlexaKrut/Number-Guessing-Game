92629d24f7769f03da273fc96d2eebf7b5c514f6
unknown <alexandra.krutihina@ya.ru>
2024-10-20 21:11:48 +0300

    Second commit!

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,49 +1,51 @@
 import random
 
-def main():
-    print("Welcome to the Number Guessing Game!")
-    print("I'm thinking of a number between 1 and 100.")
-    print("You have limited chances to guess the correct number.")
-    print()
-    print("Please select the difficulty level:")
-    print("1. Easy (10 chances)")
-    print("2. Medium (5 chances)")
-    print("3. Hard (3 chances)")
-    print()
-
+def get_level():
     levels = {1: "Easy", 2: "Medium", 3: "Hard"}
     chances = {1: 10, 2: 5, 3: 3}
-    secret_number = random.randint(1, 100)
-    count_attempts = 0
-
+    
     while True:
-        print("Enter your choice: ")
-        level = int(input())
-        if level in [1, 2, 3]:
-            attempts = chances.get(level)
-            print(f"Great! You have selected the {levels.get(level)} difficulty level.")
-            print("Let's start the game!")
-            break
+        print("Please select the difficulty level:")
+        print("1. Easy (10 chances)")
+        print("2. Medium (5 chances)")
+        print("3. Hard (3 chances)")
+        level = int(input("Enter your choice: "))
+        
+        if level in levels:
+            return levels[level], chances[level]
         else:
             print("Ooops... There's no such variant. Please, try again.")
 
+def play_game(secret_number, attempts):
+    count_attempts = 0
+
     while attempts > 0:
-        print("Enter your guess: ")
-        guess_number = int(input())
-        count_attempts += 1 
+        guess_number = int(input("Enter your guess: "))
+        count_attempts += 1
+        
         if guess_number == secret_number:
             print(f"Congratulations! You guessed the correct number in {count_attempts} attempts.")
-            break
+            return True
         elif guess_number < secret_number:
             print(f"Incorrect! The number is greater than {guess_number}.")
-        elif guess_number > secret_number:
-            print(f"Incorrect! The number is less than {guess_number}.") 
-        attempts -= 1 
+        else:
+            print(f"Incorrect! The number is less than {guess_number}.")
+        
+        attempts -= 1
 
-    if attempts == 0:
-        print("The Game is Over. You are out of chances.")
-    else:
-        print("Win!")
+    print("The Game is Over. You are out of chances.")
+    return False
+
+def main():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    
+    level, attempts = get_level()
+    print(f"Great! You have selected the {level} difficulty level.")
+    print("Let's start the game!")
+
+    secret_number = random.randint(1, 100)
+    play_game(secret_number, attempts)
 
 if __name__ == '__main__':
     main()
