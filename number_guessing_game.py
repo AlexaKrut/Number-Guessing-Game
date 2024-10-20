e23d50880b91d74b1516ec2045413e87cdb43684
unknown <alexandra.krutihina@ya.ru>
2024-10-20 21:58:00 +0300

    Third commit!

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 def get_level():
     levels = {1: "Easy", 2: "Medium", 3: "Hard"}
@@ -9,6 +10,7 @@ def get_level():
         print("1. Easy (10 chances)")
         print("2. Medium (5 chances)")
         print("3. Hard (3 chances)")
+        print()
         level = int(input("Enter your choice: "))
         
         if level in levels:
@@ -18,6 +20,7 @@ def get_level():
 
 def play_game(secret_number, attempts):
     count_attempts = 0
+    start_time = time.time()
 
     while attempts > 0:
         guess_number = int(input("Enter your guess: "))
@@ -25,7 +28,10 @@ def play_game(secret_number, attempts):
         
         if guess_number == secret_number:
             print(f"Congratulations! You guessed the correct number in {count_attempts} attempts.")
-            return True
+            end_time = time.time()
+            execution_time = end_time - start_time
+            print(f"It took {execution_time:.2f} seconds.")
+            return count_attempts  # Возвращаем количество попыток
         elif guess_number < secret_number:
             print(f"Incorrect! The number is greater than {guess_number}.")
         else:
@@ -34,18 +40,41 @@ def play_game(secret_number, attempts):
         attempts -= 1
 
     print("The Game is Over. You are out of chances.")
-    return False
+    print()
+    return None  # Возвращаем None, если не угадали
+
+def quit_the_game():
+    print("Would you like to continue? yes or no")
+    answer = input()
+    return answer.lower() == 'yes'
 
 def main():
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
+    print()
     
-    level, attempts = get_level()
-    print(f"Great! You have selected the {level} difficulty level.")
-    print("Let's start the game!")
+    best_attempts = float('inf')  # Инициализируем лучший результат как бесконечность
 
-    secret_number = random.randint(1, 100)
-    play_game(secret_number, attempts)
+    while True:
+        level, attempts = get_level()
+        print(f"Great! You have selected the {level} difficulty level.")
+        print("Let's start the game!")
+        print()
+
+        secret_number = random.randint(1, 100)
+        current_attempts = play_game(secret_number, attempts)
+
+        # Проверяем, угадал ли игрок число
+        if current_attempts is not None:
+            # Если угадал, проверяем, обновляется ли лучший результат
+            if current_attempts < best_attempts:
+                best_attempts = current_attempts
+                print(f"New best result! You guessed the number in {best_attempts} attempts.")
+            else:
+                print(f"Your best result remains {best_attempts} attempts.")
+
+        if not quit_the_game():
+            break
 
 if __name__ == '__main__':
     main()
